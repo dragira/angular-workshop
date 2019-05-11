20a10cdc996f5a01d59c8590cd567b57cda04965
Dragunova Irina (FICCP) admin <a7hqv94@eur.corp.vattenfall.com>
2019-05-11 13:14:38 +0200

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes/heroes.component';
+import { PrincessesComponent } from './princesses/princesses/princesses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'heroes' },
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'princesses',
+    component: PrincessesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
