5b396fc46f38e95077907b44ec94fca4d87b3941
Dragunova Irina (FICCP) admin <a7hqv94@eur.corp.vattenfall.com>
2019-05-12 12:15:25 +0200

    Module removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { HeroesModule } from './heroes/heroes.module';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes/heroes.component';
 import { PrincessDetailComponent } from './princesses/princess-detail/princess-detail.component';
 import { PrincessesComponent } from './princesses/princesses/princesses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
     PrincessesComponent,
     PrincessDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
-    HeroesModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
