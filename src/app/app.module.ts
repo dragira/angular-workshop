0df6f7145cbf3ef0bcc06d092e1dce10a3faf45f
Dragunova Irina (FICCP) admin <a7hqv94@eur.corp.vattenfall.com>
2019-05-12 11:06:45 +0200

    Instructions added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,21 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HeroesModule } from './heroes/heroes.module';
 import { PrincessDetailComponent } from './princesses/princess-detail/princess-detail.component';
+// import { PrincessesModule } from './princesses/princesses.module';
 import { PrincessesComponent } from './princesses/princesses/princesses.component';
+
+// 1. Create princesses module from app folder: run 'ng g m princesses' from terminal
+// 2. Insert PrincessesComponent and PrincessDetailComponent into declarations AND export in PrincessesModule
+// 3. Insert PrincessesModule into imports in AppModule (uncomment the line)
+// 4. Remove PrincessesComponent and PrincessDetailComponent from  declarations in AppModule
+
+// Bonus: routing in PrincessesModule
+// 1. Create file princesses-routing.module.ts in princesses folder
+// 2. In the new file select snippet a-module-with-routing (start typing a-module... and select from the list).
+//    Rename the module to PrincessesRoutingModule. Add RouterModule to exports.
+// 3. Import PrincessesRoutingModule in PrincessesModule.
+// 4. Use snippet a-route-path-eager inside 'const routes: Routes = [];' in PrincessesRoutingModule.
+//    Replace path by 'princesses', and component by PrincessesComponent.
 
 @NgModule({
   declarations: [
@@ -17,6 +31,7 @@ import { PrincessesComponent } from './princesses/princesses/princesses.componen
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    // PrincessesModule,
     HeroesModule
   ],
   providers: [],
