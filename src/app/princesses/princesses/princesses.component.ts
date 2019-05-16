4e85ca69785c71dc04390a7924776077477ec58a
Dragunova Irina (FICCP) admin <a7hqv94@eur.corp.vattenfall.com>
2019-05-16 23:42:29 +0200

    Instructions added

--- src/app/princesses/princesses/princesses.component.ts
+++ src/app/princesses/princesses/princesses.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PrincessesService } from '../princesses.service';
+// import { PrincessesService } from '../princesses.service';
 
 @Component({
   selector: 'app-princesses',
@@ -7,20 +7,52 @@ import { PrincessesService } from '../princesses.service';
   styleUrls: ['./princesses.component.scss']
 })
 export class PrincessesComponent implements OnInit {
-  princesses;
+  // 1. Open princesses folder in terminal and run ng g s princesses  (alias to   ng generate service princesses)
+  // 2. Move princesses to service
+  princesses = [
+    {
+      id: 1,
+      name: 'Rapunzel',
+      favoriteColor: 'Rose',
+      hair: 'Blond'
+    },
+    {
+      id: 2,
+      name: 'Ariel',
+      favoriteColor: 'Green',
+      hair: 'Red'
+    },
+    {
+      id: 3,
+      name: 'Snow White',
+      favoriteColor: 'Blue',
+      hair: 'Black'
+    }
+  ];
+
+  // princesses;
   selectedPrincess;
 
-  constructor(private princessesService: PrincessesService) {}
+  // 3. Inject PrincessesService into constructor (replace the code below)
+  constructor() {}
+  // constructor(private princessesService: PrincessesService) {}
+
+  // 4. Add getPrincesses function to ngOnInit (uncomment the code below)
 
   ngOnInit() {
-    this.getPrincesses();
+    // this.getPrincesses();
   }
 
-  getPrincesses() {
+/*   getPrincesses() {
     this.princesses = this.princessesService.getPrincesses();
-  }
+  } */
 
   selectPrincess(princess) {
     this.selectedPrincess = princess;
   }
+
+  // 5. Add getPrincesses function below to the service (remove from here)
+/*   getPrincesses() {
+    return this.princesses;
+  } */
 }
